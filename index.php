9c6888aa9fef495fd032f311a1c378be3b68dbf5
marcokunz <takuma@bluewin.ch>
2017-09-30 13:42:57 +0200

    Index File
    added Index File

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Manipake House Management</title>
+</head>
+<body>
+<?php
+?>
+/**
+ * Created by PhpStorm.
+ * User: marcokunz
+ * Date: 30.09.17
+ * Time: 13:29
+ */
