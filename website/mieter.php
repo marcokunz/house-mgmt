4fb22a9dcf9be566ee649abe87fbba884ce5b901
marcokunz <takuma@bluewin.ch>
2017-10-14 09:59:06 +0200

    UI

---
+++ website/mieter.php
@@ -0,0 +1,95 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+	<title>Typography | Klorofil - Free Bootstrap Dashboard Template</title>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+	<!-- VENDOR CSS -->
+	<link rel="stylesheet" href="assets/vendor/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/vendor/font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" href="assets/vendor/linearicons/style.css">
+	<!-- MAIN CSS -->
+	<link rel="stylesheet" href="assets/css/main.css">
+	<!-- FOR DEMO PURPOSES ONLY. You should remove this in your project -->
+	<link rel="stylesheet" href="assets/css/demo.css">
+	<!-- GOOGLE FONTS -->
+	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700" rel="stylesheet">
+	<!-- ICONS -->
+	<link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png">
+	<link rel="icon" type="image/png" sizes="96x96" href="assets/img/favicon.png">
+</head>
+
+<body>
+	<!-- WRAPPER -->
+	<div id="wrapper">
+        <!-- NAVBAR -->
+        <nav class="navbar navbar-default navbar-fixed-top">
+            <div class="brand">
+                <a href="main_page.php"><img src="" alt="ManiHacke Logo" class="img-responsive logo"></a>
+            </div>
+            <div class="container-fluid">
+                <div class="navbar-btn">
+                    <button type="button" class="btn-toggle-fullwidth"><i class="lnr lnr-arrow-left-circle"></i></button>
+                </div>
+
+
+                <div id="navbar-menu">
+                    <ul class="nav navbar-nav navbar-right">
+
+
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown"><img src="assets/img/user.png" class="img-circle" alt="Avatar"> <span>Cedi</span> <i class="icon-submenu lnr lnr-chevron-down"></i></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="main_page.php" class="active"><i class="lnr lnr-map"></i> <span>Übersicht</span></a></li>
+                                <li><a href="rechnungen.php" class=""><i class="lnr lnr-file-empty"></i> <span>Rechnungen</span></a></li>
+                                <li><a href="mieter.php" class=""><i class="lnr lnr-home"></i> <span>Mieter</span></a></li>
+                                <li><a href="nebenkosten.php" class=""><i class="lnr lnr-database"></i> <span>Nebenkosten abrechnen</span></a></li>
+                                <li><a href="index.php" class=""><i class="lnr lnr-lock"></i> <span>Logout</span></a></li>
+                            </ul>
+                        </li>
+                        <!-- <li>
+                            <a class="update-pro" href="https://www.themeineed.com/downloads/klorofil-pro-bootstrap-admin-dashboard-template/?utm_source=klorofil&utm_medium=template&utm_campaign=KlorofilPro" title="Upgrade to Pro" target="_blank"><i class="fa fa-rocket"></i> <span>UPGRADE TO PRO</span></a>
+                        </li> -->
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- END NAVBAR -->
+        <!-- LEFT SIDEBAR -->
+        <div id="sidebar-nav" class="sidebar">
+            <div class="sidebar-scroll">
+                <nav>
+                    <ul class="nav">
+                        <li><a href="main_page.php" class=""><i class="lnr lnr-map"></i> <span>Übersicht</span></a></li>
+                        <li><a href="rechnungen.php" class=""><i class="lnr lnr-file-empty"></i> <span>Rechnungen</span></a></li>
+                        <li><a href="mieter.php" class="active"><i class="lnr lnr-home"></i> <span>Mieter</span></a></li>
+                        <li><a href="nebenkosten.php" class=""><i class="lnr lnr-database"></i> <span>Nebenkosten abrechnen</span></a></li>
+                        <li><a href="index.php" class=""><i class="lnr lnr-lock"></i> <span>Logout</span></a></li>
+
+
+                    </ul>
+                </nav>
+            </div>
+        </div>
+        <!-- END LEFT SIDEBAR -->
+		<!-- MAIN -->
+
+		<!-- END MAIN -->
+		<div class="clearfix"></div>
+		<footer>
+			<div class="container-fluid">
+				<p class="copyright">&copy; 2017 <a href="https://www.themeineed.com" target="_blank">Theme I Need</a>. All Rights Reserved.</p>
+			</div>
+		</footer>
+	</div>
+	<!-- END WRAPPER -->
+	<!-- Javascript -->
+	<script src="assets/vendor/jquery/jquery.min.js"></script>
+	<script src="assets/vendor/bootstrap/js/bootstrap.min.js"></script>
+	<script src="assets/vendor/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+	<script src="assets/scripts/klorofil-common.js"></script>
+</body>
+
+</html>
