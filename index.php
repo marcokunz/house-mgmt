c20cdd23727eb3e4868ef846228618b793ab2099
marcokunz <takuma@bluewin.ch>
2017-11-03 12:37:01 +0100

    update

--- index.php
+++ index.php
@@ -61,7 +61,7 @@ Router::route("POST", "/login", function () {
     $stmt->bindValue(':email', $email);
     $stmt->execute();
     if ($stmt->rowCount() > 0) {
-        /*$agent = $stmt->fetchAll(PDO::FETCH_ASSOC)[0];
+        $agent = $stmt->fetchAll(PDO::FETCH_ASSOC)[0];
         if (password_verify($_POST["password"], $agent["password"])) {
             $_SESSION["agentLogin"]["name"] = $agent["name"];
             $_SESSION["agentLogin"]["email"] = $email;
@@ -73,7 +73,7 @@ Router::route("POST", "/login", function () {
                 $stmt->bindValue(':password', password_hash($_POST["password"], PASSWORD_DEFAULT));
                 $stmt->execute();
             }
-        }*/
+        }
     }
     Router::redirect("/");
 });
