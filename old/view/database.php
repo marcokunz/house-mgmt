f5a77acadd618eb9324dc4569a7b964ca090cca8
marcokunz <takuma@bluewin.ch>
2017-11-03 10:49:08 +0100

    update

--- old/view/database.php
+++
@@ -1,5 +0,0 @@
-<?php
-$benutzer="id3094629_housemgmtdbuser";
-$passwort="yZJ-9nZ-PBT-TQH";
-$dbname="id3094629_housemgmtdb";
-?>
--- old/view/session_check.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (!isset($_SESSION['eingeloggt']))
-{
-echo "Hallo, Sie haben keinen Zugang hier!<br/>";
-echo "<a href=\"index.php\"> Zum Login</a>";
-exit();
-}
-?>
