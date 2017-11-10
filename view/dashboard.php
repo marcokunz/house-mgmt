95069f5f49b07688e7d83a173d8e268c8671307e
patricktuescher <patrick.tuescher@students.fhnw.ch>
2017-11-10 16:59:56 +0100

    update dashboard

--- view/dashboard.php
+++ view/dashboard.php
@@ -12,37 +12,3 @@ use domain\Rechnungen;
 ?>
 
 
-
-
-
-<div class="container">
-    <div class="page-header">
-        <h2 class="text-center"><strong>Rechnungen</strong>.</h2></div>
-    <div class="table-responsive">
-        <table class="table">
-            <thead>
-            <tr>
-                <th>Datum</th>
-                <th>Typ</th>
-                <th>Betrag</th>
-
-            </tr>
-            </thead>
-            <tbody>
-            <?php
-
-            global $rechnung;
-            foreach($rechnung as $rechnungen):
-                ?>
-
-                <tr>
-                    <td><?php echo $rechnungen->getDatum(); ?></td>
-                    <td><?php echo $rechnungen->getTyp(); ?> </td>
-                    <td><?php echo $rechnungen->getBetrag(); ?> </td>
-                </tr>
-            <?php endforeach; ?>
-
-
-            </tbody>
-        </table>
-    </div>
