a492b9a442199146e45e5ea940b846ea78d1e44d
marcokunz <takuma@bluewin.ch>
2017-11-11 10:56:44 +0100

    update

--- index.php
+++ index.php
@@ -123,7 +123,7 @@ Router::route_auth("POST", "/rechnungen/update", $authFunction, function () {
     $rechnungenDAO->create($rechnung);
 
 
-    layoutSetContent("rechnungen.php");
+    Router::redirect("/rechnungen");
 });
 
 Router::route_auth("GET", "/einnahmen", $authFunction, function () {
