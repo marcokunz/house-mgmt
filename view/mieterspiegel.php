9ef1abfe8940a35ead682c6a3c83e5cd15df6169
burri44 <scui.brunotrittibach@hotmail.com>
2017-11-17 17:27:55 +0100

    div
    div

--- view/mieterspiegel.php
+++ view/mieterspiegel.php
@@ -6,10 +6,7 @@
  * Time: 16:59
  */
 
-use view\View;
-use dao\MieterDAO;
-use domain\Mieter;
-require_once("config/Autoloader.php");
+
 require("fpdf/pdf.php");
 $pdf = new FPDF();
 $pdf->AddPage();
