07f75ab8e7e33c26fc24fbcae4b6fa9656762e06
burri44 <scui.brunotrittibach@hotmail.com>
2017-11-17 21:40:30 +0100

    total
    total

--- view/mieterspiegel.php
+++ view/mieterspiegel.php
@@ -35,16 +35,21 @@ class PDF extends FPDF
         $this->SetFont('');
         // Data
         $fill = false;
+        $total = 0;
         foreach($data as $row)
         {
             $this->Cell($w[0],6,iconv('UTF-8', 'windows-1252',$row->getVorname()),'LR',0,'L',$fill);
             $this->Cell($w[1],6,iconv('UTF-8', 'windows-1252',$row->getNachname()),'LR',0,'L',$fill);
             $this->Cell($w[2],6,iconv('UTF-8', 'windows-1252',$row->getAdresse()),'LR',0,'L',$fill);
             $this->Cell($w[3],6,iconv('UTF-8', 'windows-1252',$row->getMietzins()),'LR',0,'L',$fill);
-
+            $total += $row->getMietzins();
             $this->Ln();
             $fill = !$fill;
         }
+        $this->Cell($w[0],6,"TOTAL",'LR',0,'L',$fill);
+        $this->Cell($w[1],6,"",'LR',0,'L',$fill);
+        $this->Cell($w[2],6,"",'LR',0,'L',$fill);
+        $this->Cell($w[3],6,$total,'LR',0,'L',$fill);
         // Closing line
         $this->Cell(array_sum($w),0,'','T');
     }
