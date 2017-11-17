3e2ffe67cf6d47ce67735e689eff3db93c43debe
burri44 <scui.brunotrittibach@hotmail.com>
2017-11-17 17:30:09 +0100

    div
    div

--- view/mieterspiegel.php
+++ view/mieterspiegel.php
@@ -8,10 +8,11 @@
 
 
 require("fpdf/pdf.php");
-$pdf = new FPDF();
+$pdf = new FPDF('P','mm','A4');
 $pdf->AddPage();
 $pdf->SetFont('Arial','B',16);
 $pdf->Cell(40,10,'Hello World!');
+$pdf->Cell(60,10,'Powered by FPDF.',0,1,'C');
 $pdf->Output();
 ?>
 
