3e307560c8257bfc9a4053c302c0d4aa7af067fc
marcokunz <takuma@bluewin.ch>
2017-11-17 12:49:25 +0100

    update

--- view/abrechnungen.php
+++ view/abrechnungen.php
@@ -18,7 +18,16 @@ require_once("config/Autoloader.php");
         <?php
     global $mieter;
     foreach($mieter as $mietertabelle): ?>
+
+        <?php
+        $counter = 0;
+        if(counter%3==0):
+            $counter++;?>
     <div class="row" style="margin:0px;padding:50px;">
+
+    <?php endif; ?>
+
+    <!--<div class="row" style="margin:0px;padding:50px;">-->
         <div class="col-md-4" style="background-color:rgba(227,221,221,0);">
             <h4 class="text-left"><?php echo $mietertabelle->getVorname()." ".$mietertabelle->getNachname() ;?></h4>
             <div class="table-responsive">
