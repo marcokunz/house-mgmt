60ea98a29aadae4eeb5df78b782ea2b32f601558
marcokunz <takuma@bluewin.ch>
2017-11-17 12:32:03 +0100

    update

--- view/abrechnungen.php
+++ view/abrechnungen.php
@@ -14,12 +14,11 @@ require_once("config/Autoloader.php");
 <div class="container">
     <div class="page-header">
         <h2 class="text-center"><strong>Abrechnungen</strong> erstellen.</h2></div>
-
-    <div class="row" style="margin:0px;padding:50px;">
+    
         <?php
     global $mieter;
     foreach($mieter as $mietertabelle): ?>
-    <!--<div class="row" style="margin:0px;padding:50px;">-->
+    <div class="row" style="margin:0px;padding:50px;">
         <div class="col-md-4" style="background-color:rgba(227,221,221,0);">
             <h4 class="text-left"><?php echo $mietertabelle->getNachname();?></h4>
             <div class="table-responsive">
