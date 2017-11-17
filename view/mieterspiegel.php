4c90d30fd54d04c36923ea34eeb65239a04ced31
burri44 <scui.brunotrittibach@hotmail.com>
2017-11-17 21:26:33 +0100

    div
    div

--- view/mieterspiegel.php
+++ view/mieterspiegel.php
@@ -10,51 +10,10 @@ class PDF extends FPDF
 // Load data
     function LoadData()
     {
-        // Read file lines
         global $mieter;
-        $data = array();
-        $data = $mieter;
-
-        return $data;
+        return $mieter;
     }
 
-// Simple table
-    function BasicTable($header, $data)
-    {
-        // Header
-        foreach($header as $col)
-            $this->Cell(40,7,$col,1);
-        $this->Ln();
-        // Data
-        foreach($data as $row)
-        {
-            foreach($row as $col)
-                $this->Cell(40,6,$col,1);
-            $this->Ln();
-        }
-    }
-
-// Better table
-    function ImprovedTable($header, $data)
-    {
-        // Column widths
-        $w = array(40, 35, 40, 45);
-        // Header
-        for($i=0;$i<count($header);$i++)
-            $this->Cell($w[$i],7,$header[$i],1,0,'C');
-        $this->Ln();
-        // Data
-        for($i = 0; i < $data.count(); $i++)
-        {
-            $this->Cell($w[0],6,$data[0],'LR');
-            $this->Cell($w[1],6,$data[1],'LR');
-            $this->Cell($w[2],6,$data[2],'LR',0,'R');
-            $this->Cell($w[3],6,$data[3],'LR',0,'R');
-            $this->Ln();
-        }
-        // Closing line
-        $this->Cell(array_sum($w),0,'','T');
-    }
 
 // Colored table
     function FancyTable($header, $data)
@@ -79,9 +38,9 @@ class PDF extends FPDF
         foreach($data as $row)
         {
             $this->Cell($w[0],6,$row->getVorname(),'LR',0,'L',$fill);
-            $this->Cell($w[0],6,$row->getNachname(),'LR',0,'L',$fill);
-            $this->Cell($w[0],6,$row->getAdresse(),'LR',0,'L',$fill);
-            $this->Cell($w[0],6,$row->getMietzins(),'LR',0,'L',$fill);
+            $this->Cell($w[1],6,$row->getNachname(),'LR',0,'L',$fill);
+            $this->Cell($w[2],6,$row->getAdresse(),'LR',0,'L',$fill);
+            $this->Cell($w[3],6,$row->getMietzins(),'LR',0,'L',$fill);
 
             $this->Ln();
             $fill = !$fill;
