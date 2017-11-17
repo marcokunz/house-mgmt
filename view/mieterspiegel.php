b02e8edaafc48a9e8b19fc208591e81780a08d6b
burri44 <scui.brunotrittibach@hotmail.com>
2017-11-17 19:05:46 +0100

    div
    div

--- view/mieterspiegel.php
+++ view/mieterspiegel.php
@@ -51,8 +51,8 @@ class PDF extends FPDF
         {
             $this->Cell($w[0],6,$row[0],'LR');
             $this->Cell($w[1],6,$row[1],'LR');
-            $this->Cell($w[2],6,number_format($row[2]),'LR',0,'R');
-            $this->Cell($w[3],6,number_format($row[3]),'LR',0,'R');
+            $this->Cell($w[2],6,$row[2],'LR',0,'R');
+            $this->Cell($w[3],6,$row[3],'LR',0,'R');
             $this->Ln();
         }
         // Closing line
