1cd152201c124eda5b6198a61e84b7ce24231960
burri44 <scui.brunotrittibach@hotmail.com>
2017-11-17 17:21:42 +0100

    test
    test

--- view/mieterspiegel.php
+++ view/mieterspiegel.php
@@ -10,6 +10,12 @@ use view\View;
 use dao\MieterDAO;
 use domain\Mieter;
 require_once("config/Autoloader.php");
+require_once("fpdf/pdf.php");
+$pdf = new FPDF();
+$pdf->AddPage();
+$pdf->SetFont('Arial','B',16);
+$pdf->Cell(40,10,'Hello World!');
+$pdf->Output();
 ?>
 
 
