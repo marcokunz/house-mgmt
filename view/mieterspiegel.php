02d218df3a19eaf32f7cb31aa0df6d196551922b
burri44 <scui.brunotrittibach@hotmail.com>
2017-11-17 17:38:31 +0100

    div
    div

--- view/mieterspiegel.php
+++ view/mieterspiegel.php
@@ -1,20 +1,9 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: andreas.martin
- * Date: 13.09.2017
- * Time: 16:59
- */
+require('fpdf.php');
 
-
-require("fpdf/fpdf.php");
-echo "hello";
-$pdf = new FPDF('P','mm','A4');
+$pdf = new FPDF();
 $pdf->AddPage();
 $pdf->SetFont('Arial','B',16);
 $pdf->Cell(40,10,'Hello World!');
-$pdf->Cell(60,10,'Powered by FPDF.',0,1,'C');
 $pdf->Output();
 ?>
-
-
