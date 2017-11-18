4d9bf259828e619ad59b3c62d12cfb508765c16d
burri44 <scui.brunotrittibach@hotmail.com>
2017-11-18 10:50:19 +0100

    div
    div

--- view/mieterspiegelPDF.php
+++ view/mieterspiegelPDF.php
@@ -12,12 +12,14 @@ class PDF extends FPDF
     function Header()
     {
         // Arial bold 15
-        $this->SetFont('Arial','',30);
-        $this->SetTextColor(0, 0, 255);
+        $this->SetFont('Helvetica','',30);
+        $this->SetTextColor(255, 255, 255);
         // Move to the right
         $this->Cell(80);
         // Title
-        $this->Cell(30,10,'Dein <b>Mieterspiegel</b>.',0,0,'C');
+        $this->Cell(30,10,'Dein Mieterspiegel.',0,0,'C');
+        $this->SetFont('Helvetica','B',30);
+        $this->Cell(30,10,'Mieterspiegel.',0,0,'C');
         // Line break
         $this->Ln(20);
     }
