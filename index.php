390d740fe17fd98c6dcddc48051addd005def6f0
marcokunz <takuma@bluewin.ch>
2017-11-21 19:18:23 +0100

    update

--- index.php
+++ index.php
@@ -111,11 +111,11 @@ Router::route_auth("GET", "/", $authFunction, function () {
     layoutSetContent("dashboard.php");
 });
 
-/*//Einnahmen
+//Einnahmen
 
-Router::route_auth("GET", "/einnahmen", function () {
-    require_once("view/einnahmen.php");
-});*/
+Router::route("GET", "/einnahmen", function () {
+    layoutSetContent("view/einnahmen.php");
+});
 
 // Mieter
 
