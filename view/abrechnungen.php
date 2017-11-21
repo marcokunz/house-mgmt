3c6d2d64fa07ce7ef0b7c2835d15b94e2bc0f7df
marcokunz <takuma@bluewin.ch>
2017-11-21 14:13:29 +0100

    update

--- view/abrechnungen.php
+++ view/abrechnungen.php
@@ -31,7 +31,7 @@ require_once("config/Autoloader.php");
 
     <?php endif; ?>
 
-        <div class="col-md-4" style="background-color: transparent; padding: 10px; border-color: lightgrey; border-width: thin; border-style: solid; border-radius: 10px">
+        <div class="col-md-4" style="background-color: lightgrey; border-color: white; border-width: thick; border-style: solid; border-radius: 10px">
             <h4 class="text-left"><?php echo $mietertabelle->getVorname()." ".$mietertabelle->getNachname() ;?></h4>
             <div class="table-responsive">
                 <table class="table">
