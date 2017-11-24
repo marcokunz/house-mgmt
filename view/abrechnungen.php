4001956360f970069685e2090f942642012488a8
patricktuescher <patrick.tuescher@students.fhnw.ch>
2017-11-24 11:28:29 +0100

    abrech

--- view/abrechnungen.php
+++ view/abrechnungen.php
@@ -55,8 +55,8 @@ require_once("config/Autoloader.php");
                         <td><?php echo $KostenDAO->getTotalKosten($mietertabelle->getId(), "Nebenkosten")?></td>
                     </tr>
                     <tr>
-                        <td>Mietzins </td>
-                        <td><?php echo $einnahme->getTotalEinnahmen($mietertabelle->getId(), "Mieteingänge")?></td>
+                        <td>Mieteingänge </td>
+                        <td><?php echo $einnahme->getTotalEinnahmen($mietertabelle->getId())?></td>
                     </tr>
                     <tr>
                         <td> <a target="_blank" class="btn btn-primary btn-sm" role="button" href="customer/pdf"> <i class="fa fa-file-pdf-o"></i></a></td>
