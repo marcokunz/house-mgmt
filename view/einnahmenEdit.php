e3d0dbddc9fb710087a0c7d0fe58ad61f4152136
marcokunz <takuma@bluewin.ch>
2017-11-24 14:04:44 +0100

    update

--- view/einnahmenEdit.php
+++ view/einnahmenEdit.php
@@ -44,7 +44,7 @@ use domain\Mieter;
             </div>
         </div>
         <div class="btn-group" role="group">
-            <button class="btn btn-default" type="submit"> <i class="fa fa-save"></i></button>
+            <button class="btn btn-success" type="submit"> <i class="fa fa-save"></i></button>
         </div>
     </form>
 </div>
