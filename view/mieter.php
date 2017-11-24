e66de58e435860887c2ec42ad72a90d8a9a4dba2
marcokunz <takuma@bluewin.ch>
2017-11-24 12:51:04 +0100

    update

--- view/mieter.php
+++ view/mieter.php
@@ -34,8 +34,8 @@ require_once("config/Autoloader.php");
                 <tr>
                     <td><?php echo $mietertabelle->getVorname();?></td>
                     <td><?php echo $mietertabelle->getNachname();?> </td>
-                    <td><?php echo $mietertabelle->getQuadratmeter();?> </td>
-                    <td><?php echo $mietertabelle->getMietzins();?> </td>
+                    <td><?php echo $mietertabelle->getQuadratmeter()." m &#xB2";?> </td>
+                    <td><?php echo "CHF ".$mietertabelle->getMietzins();?> </td>
                     <td>
                         <div class="btn-group btn-group-sm" role="group">
                             <a class="btn btn-warning" role="button" href="mieter/edit?id=<?php echo $mietertabelle->getId(); ?>"> <i class="fa fa-edit"></i></a>
