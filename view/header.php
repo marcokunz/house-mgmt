18fe242e07e25d7aaf043d6c5a6d7c00469cfbe4
burri44 <scui.brunotrittibach@hotmail.com>
2017-11-25 14:27:18 +0100

    div
    div

--- view/header.php
+++ view/header.php
@@ -19,7 +19,7 @@ use domain\Mieter;
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>House-MGMT</title>
+    <title>Dini Mueter</title>
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic">
     <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
