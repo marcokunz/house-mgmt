4a33d5c455d1515ddd776f3fa14d520ae3daa6fa
burri44 <scui.brunotrittibach@hotmail.com>
2017-11-25 14:54:05 +0100

    div
    div

--- view/header.php
+++ view/header.php
@@ -29,7 +29,7 @@ use domain\Mieter;
     <link rel="stylesheet" href="assets/css/Login-Form-Dark.css">
     <link rel="stylesheet" href="assets/css/Lightbox-Gallery.css">
 
-    //favicon
+    <!--//favicon-->
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
