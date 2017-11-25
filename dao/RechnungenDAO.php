bbd728cb75d55f22d81ff9b2b1549aebd7f581e9
patricktuescher <patrick.tuescher@students.fhnw.ch>
2017-11-25 09:51:25 +0100

    update

--- dao/RechnungenDAO.php
+++ dao/RechnungenDAO.php
@@ -101,7 +101,7 @@ class RechnungenDAO extends BasicDAO {
         foreach ($mieter as $mietertabelle) {
             $kosten = new Kosten();
             //Betrag berechnen
-            $betrag = number_format($rechnungsbetrag / $totalgroesse * $mietertabelle->getQuadratmeter(), 2);
+            $betrag = floatval($rechnungsbetrag / $totalgroesse * $mietertabelle->getQuadratmeter());
             //$betrag = 100;
             $kosten->setBetrag($betrag);
             $kosten->setRechnungen_fk($currentRechnung);
