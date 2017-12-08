8fb7d0f07c36bf706d1861f732cdca4a5dc4c9e6
burri44 <scui.brunotrittibach@hotmail.com>
2017-12-08 14:35:39 +0100

    div
    div

--- view/mieterspiegelPDF.php
+++ view/mieterspiegelPDF.php
@@ -14,9 +14,7 @@ class PDF extends FPDF
     {
         $this->SetFont('Helvetica','',30);
         // Title
-        $this->Cell(0,0,'Dein ',0,0);
-        $this->SetFont('Helvetica','B','');
-        $this->Cell(20,0,'Mieterspiegel.',0,0, 'L');
+        $this->Cell(0,0,'Dein Mieterspiegel',0,0, 'C');
 
         // Line break
         $this->Ln(30);
